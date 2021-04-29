d90ec278873551a6d03f2765164d864063069655
vishnukumar.ps@outlook.com <vishnukumar.ps@outlook.com>
2021-04-30 01:39:57 +0530

    Model added

---
+++ JWT_Learning/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWT_Learning.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IEnumerable<string> Get()
+            => new String[] { "Vishnu","Vipin"};
+    }
+}
