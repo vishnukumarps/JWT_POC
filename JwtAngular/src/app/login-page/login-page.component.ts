2ddf785adff97eea7af36d8232dc6df7c72ca51f
vishnukumar.ps@outlook.com <vishnukumar.ps@outlook.com>
2021-04-30 02:30:23 +0530

    npm install @auth0/angular-jwt@4.2.0

---
+++ JwtAngular/src/app/login-page/login-page.component.ts
@@ -0,0 +1,38 @@
+import { HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent implements OnInit {
+  http: any;
+  invalidLogin: boolean;
+  router: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  login(form: NgForm) {
+    const credentials = JSON.stringify(form.value);
+    this.http.post("http://localhost:5000/api/auth/login", credentials, {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    }).subscribe(response => {
+      const token = (<any>response).token;
+      localStorage.setItem("jwt", token);
+      this.invalidLogin = false;
+      this.router.navigate(["/"]);
+    }, err => {
+      this.invalidLogin = true;
+    });
+  }
+  
+  logOut() {
+    localStorage.removeItem("jwt");
+  }
+}
