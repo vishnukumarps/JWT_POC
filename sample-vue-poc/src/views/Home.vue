f20e5b07971441e85dbc33c1c5c0460f6378e0a1
vishnu <vishnu.kumarps@claysys.net>
2021-05-21 10:28:44 +0530

    Wave Ui added

--- sample-vue-poc/src/views/Home.vue
+++ sample-vue-poc/src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <w-app>
+    <div class="home">
+      <img alt="Vue logo" src="../assets/logo.png" />
+    </div>
+
+    <w-flex class="wrap">
+      <w-button class="ma1" bg-color="success">success</w-button>
+      <w-button class="ma1" bg-color="error">error</w-button>
+      <w-button class="ma1" bg-color="warning">warning</w-button>
+      <w-button class="ma1" bg-color="info">info</w-button>
+      <w-button class="ma1" bg-color="primary">primary</w-button>
+      <w-button class="ma1" bg-color="secondary">secondary</w-button>
+      <w-button class="ma1" bg-color="purple-light4">purple-light4</w-button>
+    </w-flex>
+  </w-app>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
